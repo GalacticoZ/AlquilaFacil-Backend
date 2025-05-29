5f8807a96dd06be832d1a3fe791f5e7bb3bf820c
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2025-05-29 15:29:21 -0500

    feat: add docker-compose file including kafka

---
+++ Services/IAMService/IAMService/Interfaces/REST/UsersController.cs
@@ -0,0 +1,97 @@
+using System.Net.Mime;
+using IAMService.Domain.Model.Queries;
+using IAMService.Domain.Services;
+using IAMService.Infrastructure.Pipeline.Middleware.Attributes;
+using IAMService.Interfaces.REST.Resources;
+using IAMService.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAMService.Interfaces.REST;
+
+/**
+ * <summary>
+ *     The users controller
+ * </summary>
+ * <remarks>
+ *     This class is used to handle user requests
+ * </remarks>
+ */
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class UsersController(
+    IUserQueryService userQueryService, IUserCommandService userCommandService
+    ) : ControllerBase
+{
+    
+    
+    /**
+     * <summary>
+     *     Get user by id endpoint. It allows to get a user by id
+     * </summary>
+     * <param name="userId">The user id</param>
+     * <returns>The user resource</returns>
+     */
+    //[AuthorizeRole(EUserRoles.Admin)]
+    [HttpGet("{userId:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetUserById(int userId)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var user = await userQueryService.Handle(getUserByIdQuery);
+        var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user!);
+        return Ok(userResource);
+    }
+    
+    
+    /**
+     * <summary>
+     *     Get all users endpoint. It allows to get all users
+     * </summary>
+     * <returns>The user resources</returns>
+     */
+    [HttpGet]
+    //[AuthorizeRole(EUserRoles.Admin)]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var getAllUsersQuery = new GetAllUsersQuery();
+        var users = await userQueryService.Handle(getAllUsersQuery);
+        var userResources = users.Select(UserResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(userResources);
+    }
+    
+    [HttpGet("get-username/{userId:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetUsernameById(int userId)
+    {
+        var getUsernameByIdQuery = new GetUsernameByIdQuery(userId);
+        var username = await userQueryService.Handle(getUsernameByIdQuery);
+        return Ok(username);
+    }
+    
+    [HttpPut("{userId:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> UpdateUser(int userId, [FromBody] UpdateUsernameResource updateUsernameResource)
+    {
+        var updateUserCommand =
+            UpdateUsernameCommandFromResourceAssembler.ToUpdateUsernameCommand(userId, updateUsernameResource);
+        var user = await userCommandService.Handle(updateUserCommand);
+        var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user!);
+        return Ok(userResource);
+    }
+    
+    [HttpGet("user-exists/{userId:int}")]
+    [AllowAnonymous]
+    public IActionResult IsUserExists([FromRoute] int userId)
+    {
+        var userExistsQuery = new UserExistsQuery(userId);
+        var exists = userQueryService.Handle(userExistsQuery);
+        if (exists) {
+            return Ok(exists);
+        }
+        return NotFound(new { message = "User does not exist." });
+    }
+}
---
+++ Services/IAMService/IAMService/Program.cs
@@ -0,0 +1,144 @@
+using IAMService.Application.External.OutboundServices;
+using IAMService.Application.Internal.CommandServices;
+using IAMService.Application.Internal.OutboundServices;
+using IAMService.Application.Internal.QueryServices;
+using IAMService.Domain.Model.Commands;
+using IAMService.Domain.Repositories;
+using IAMService.Domain.Services;
+using IAMService.Infrastructure.Hashing.BCrypt.Services;
+using IAMService.Infrastructure.Persistence.EFC.Respositories;
+using IAMService.Infrastructure.Tokens.JWT.Configuration;
+using IAMService.Infrastructure.Tokens.JWT.Services;
+using IAMService.Interfaces.ACL.Facades;
+using IAMService.Interfaces.ACL.Facades.Service;
+using IAMService.Shared.Domain.Repositories;
+using IAMService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using IAMService.Shared.Infrastructure.Persistence.EFC.Repositories;
+using IAMService.Shared.Interfaces.ASP.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers( options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var developmentString = builder.Configuration.GetConnectionString("DevelopmentConnection");
+
+// Configure Database Context and Logging Levels
+
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            options.UseMySql(developmentString, ServerVersion.AutoDetect(developmentString))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+        else if (builder.Environment.IsProduction())
+        {
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .LogTo(Console.WriteLine, LogLevel.Error)
+                .EnableDetailedErrors();
+        }
+    });
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title = "IAMService.API",
+                Version = "v1",
+                Description = "IAM Service API",
+                TermsOfService = new Uri("https://alquila-facil.com/tos"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Alquila Fácil",
+                    Email = "contact@alquilaf.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            });
+    });
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Add CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+builder.Services.AddScoped<ISeedUserRoleCommandService, SeedUserRoleCommandService>();
+builder.Services.AddScoped<ISeedAdminCommandService, SeedAdminCommandService>();
+
+builder.Services.Configure<TokenSettings>(builder.Configuration.GetSection("TokenSettings"));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserCommandService, UserCommandService>();
+builder.Services.AddScoped<IUserQueryService, UserQueryService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IHashingService, HashingService>();
+builder.Services.AddScoped<IIamContextFacade, IamContextFacade>();
+builder.Services.AddScoped<IProfilesUserExternalService, ProfilesUserExternalService>();
+builder.Services.AddScoped<IProfilesContextFacade, ProfilesContextFacade>();
+
+builder.Services.AddHttpClient();
+
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8011);
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+    
+    var userRoleCommandService = services.GetRequiredService<ISeedUserRoleCommandService>();
+    await userRoleCommandService.Handle(new SeedUserRolesCommand());
+    
+    var adminCommandService = services.GetRequiredService<ISeedAdminCommandService>();
+    await adminCommandService.Handle(new SeedAdminCommand());
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAllPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
