4a1f075e30fce0d94cbcbcce36aff74646b6fa78
Omar Morales <andrewmm45333@gmail.com>
2025-06-06 20:36:55 -0500

    feat: plan controller with documentation added

--- Services/SubscriptionsService/SubscriptionsService/Interfaces/REST/PlanController.cs
+++ Services/SubscriptionsService/SubscriptionsService/Interfaces/REST/PlanController.cs
@@ -1,21 +1,33 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using SubscriptionsService.Domain.Model.Queries;
 using SubscriptionsService.Domain.Services;
+using SubscriptionsService.Interfaces.REST.Resources;
 using SubscriptionsService.Interfaces.REST.Transform;
+using Shared.Interfaces.REST.Resources;
 
 namespace SubscriptionsService.Interfaces.REST;
 
 [ApiController]
 [Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
 public class PlanController(IPlanQueryService planQueryService) : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(PlanResource), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponseResource), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetAllPlans()
     {
-        var getAllPlansQuery = new GetAllPlansQuery();
-        var plans = await planQueryService.Handle(getAllPlansQuery);
-        var resources = plans.Select(PlanResourceFromEntityAssembler.ToResourceFromEntity);
-        return Ok(resources);
+        try
+        {
+            var getAllPlansQuery = new GetAllPlansQuery();
+            var plans = await planQueryService.Handle(getAllPlansQuery);
+            var resources = plans.Select(PlanResourceFromEntityAssembler.ToResourceFromEntity);
+            return Ok(resources);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { Error = ex.Message }); // 404
+        }
     }
-
 }
